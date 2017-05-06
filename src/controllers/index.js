53ea62db5e82d658d7ed9134d6e9af61de5c1863
michaelsflynn <msflynn25@gmail.com>
2017-05-06 14:37:06 +0530

    Added Categories Controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,6 @@
 // Controllers Index
 exports.articles = require('./articles')
 exports.api = require('./api')
+exports.categories = require('./categories')
 exports.sources = require('./sources')
 exports.topArticles = require('./topArticles')
