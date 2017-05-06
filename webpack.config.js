b8ad7a44481dc3cc21d3671064f0f3660df7d001
michaelsflynn <msflynn25@gmail.com>
2017-05-06 14:41:22 +0530

    Initial Config for Webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// webpack.config.js for JSX and CSS
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: path.join(__dirname, '/src/index.html'),
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, include: path.join(__dirname, '/src'), loader: 'babel-loader'},
+      {test: /\.json$/, include: path.join(__dirname, '/src'), loader: 'json-loader'},
+      {test: /\.css$/, loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'}
+    ]
+  },
+  output: {
+    filename: 'app_bundle.js',
+    path: __dirname + '/dist',
+    publicPath: '/dist'
+  },
+  plugins: [HTMLWebpackPluginConfig],
+  node: {
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
+  }
+}
