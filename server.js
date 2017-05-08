b98964c0044a997e2e36a59b6b67ee56af317b85
michaelsflynn <msflynn25@gmail.com>
2017-05-08 10:09:08 +0530

    Changed 'app' to 'server' and Added express.static('dist')

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
-const app = express()
+const server = express()
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/tldrdb')
@@ -9,30 +9,30 @@ mongoose.Promise = require('promise')
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'db connection error:'))
 db.once('open', () => {
-  app.listen(process.env.PORT || 3000)
+  server.listen(process.env.PORT || 3000)
   console.log('DB connected successfully and APP listening at: ' + Date())
 })
 
-// const models = require('./src/models')
+// Controller files for API and Data Management
 const controllers = require('./src/controllers')
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({extended: true}))
 
-app.use('/dist', express.static('dist'))
+server.use(express.static('dist'))
 
-app.get('/articles', controllers.articles.get)
-app.post('/articles', controllers.articles.post)
-app.delete('/articles/:id', controllers.articles.del)
-app.put('/articles/:id', controllers.articles.upd)
+server.get('/articles', controllers.articles.get)
+server.post('/articles', controllers.articles.post)
+server.delete('/articles/:id', controllers.articles.del)
+server.put('/articles/:id', controllers.articles.upd)
 
-app.get('/sources', controllers.sources.get)
-app.get('/topArticles', controllers.topArticles.get)
-app.get('/categories', controllers.categories.get)
-app.get('/api/:data', controllers.api.get)
+server.get('/sources', controllers.sources.get)
+server.get('/topArticles', controllers.topArticles.get)
+server.get('/categories', controllers.categories.get)
+server.get('/api/:data', controllers.api.get)
 
-app.get('/', function (req, res) {
+server.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-module.exports = app
+module.exports = server
