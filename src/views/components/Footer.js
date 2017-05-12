8263a220baf9a8d4186d89b8e4e1b70daebe1ef2
michaelsflynn <msflynn25@gmail.com>
2017-05-12 17:44:34 +0530

    Added Header and Footer Components

---
+++ src/views/components/Footer.js
@@ -0,0 +1,15 @@
+// Component - Main App
+
+import React from 'react'
+
+class Footer extends React.Component {
+  render () {
+    return (
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      <h5 >Powered by newsAPI.org</h5>
+    </div>
+    )
+  }
+}
+
+export default Footer
---
+++ src/views/components/Header.js
@@ -0,0 +1,15 @@
+// Component - Main App
+
+import React from 'react'
+
+class Header extends React.Component {
+  render () {
+    return (
+    <div>
+      <h1>Articly: Your Source for Top News</h1>
+    </div>
+    )
+  }
+}
+
+export default Header
