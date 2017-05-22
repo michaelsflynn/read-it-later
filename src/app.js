633f70dec3afe49f6e5f59281e3c749c5165f357
michaelsflynn <msflynn25@gmail.com>
2017-05-22 10:14:28 +0530

    Changed primary component from App.js to Main.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-// Index.js in Views = Main App Page
+// app.js in src = Entry Point/Initial Render
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import appReducers from './views/reducers'
-import App from './views/components/App'
+import Main from './views/components/Main'
+
+import './scss/index.scss'
 
 // Grab the state from a global variable injected into the server-generated HTML
 const preloadedState = window.__PRELOADED_STATE__
@@ -15,9 +17,10 @@ delete window.__PRELOADED_STATE__
 // Create Redux store with initial state
 const store = createStore(appReducers, preloadedState)
 
+// Initial Render: ReactDOM.Render()
 render(
   <Provider store={store}>
-    <App />
+    <Main />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('content')
 )
