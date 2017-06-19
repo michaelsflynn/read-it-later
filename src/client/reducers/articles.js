b8fb741caf7735d9e9e2dab535514503fb34cc25
michaelsflynn <msflynn25@gmail.com>
2017-06-19 09:24:51 +0530

    Modified to add saved articles data

--- src/client/reducers/articles.js
+++ src/client/reducers/articles.js
@@ -1,11 +1,15 @@
 // Reducers for Articles
-const initialState = { showArticles: false, articles: [] }
+const initialState = { showArticles: false, articles: [], savedArticles: [] }
 
 const setArticles = (state = initialState, action) => {
   switch (action.type) {
     case 'SET_ARTICLES':
       return Object.assign({}, state, {
         articles: action.articles
+      })
+    case 'SET_SAVED_ARTICLES':
+      return Object.assign({}, state, {
+        savedArticles: action.savedArticles
       })
     case 'SHOW_ARTICLES':
       return Object.assign({}, state, {
